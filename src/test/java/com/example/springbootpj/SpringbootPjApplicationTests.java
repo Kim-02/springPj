cc62bbcb305a40cdd4ee468dab0e00ca79fd53f4
Kim-02 <curry2002@koreatech.ac.kr>
2025-01-11 17:44:23 +0900

    firstcommit

---
+++ src/test/java/com/example/springbootpj/SpringbootPjApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.springbootpj;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringbootPjApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
