6b943ce00dcdfeff2767001479ac27a4900d9a29
김승환 <curry2002@naver.com>
2025-01-24 17:18:40 +0900

    로그인 시 정보 검증 후 토큰 발급 완료

--- src/main/java/com/dasolsystem/core/auth/signin/controller/signinController.java
+++ src/main/java/com/dasolsystem/core/auth/signin/controller/signinController.java
@@ -5,6 +5,7 @@ import com.dasolsystem.core.auth.signin.Dto.ResponseSignincheckDto;
 import com.dasolsystem.core.auth.signin.service.signinService;
 import com.dasolsystem.core.jwt.util.JwtBuilder;
 import com.dasolsystem.core.post.Dto.ResponseJson;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,22 +18,23 @@ public class signinController {
     private final JwtBuilder jwtBuilder;
     @PostMapping
     public ResponseEntity<ResponseJson<Object>> signin(
-            @RequestBody RequestSignincheckDto signincheckDto
+            @RequestBody RequestSignincheckDto signincheckDto,
+            HttpServletResponse res
             ){
         String jwtToken = null;
         ResponseSignincheckDto response = service.loginCheck(signincheckDto);
         if(response.getState().value){
             jwtToken = jwtBuilder.generateAccessToken(response.getName());
+            res.setHeader("Content-Type", "application/json");
+            res.setHeader("Authorization", jwtToken);
         }
         return ResponseEntity.ok(
                 ResponseJson.builder()
                         .status(200)
                         .message("JWT")
-                        .result(jwtToken)
+                        .result(res.getHeader("Authorization"))
                         .build()
         );
     }
-
-
 
 }
