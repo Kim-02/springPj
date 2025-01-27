3cc80298ed606062cd8ef82d2024307197f48c8f
Kim-02 <80444565+Kim-02@users.noreply.github.com>
2025-01-27 17:33:03 +0900

    자잘한 오류 수정 및 index변경

--- src/main/java/com/dasolsystem/core/auth/signup/controller/signUpController.java
+++ src/main/java/com/dasolsystem/core/auth/signup/controller/signUpController.java
@@ -1,5 +1,6 @@
 package com.dasolsystem.core.auth.signup.controller;
 
+import com.dasolsystem.config.MvcConfiguration;
 import com.dasolsystem.core.post.Dto.ResponseJson;
 import com.dasolsystem.core.auth.signup.dto.RequestSignupPostDto;
 import com.dasolsystem.core.auth.signup.dto.ResponseSavedNameDto;
@@ -7,20 +8,17 @@ import com.dasolsystem.core.auth.signup.service.signupService;
 import jdk.jfr.Description;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/signup")
-public class signUpController {
+public class signUpController extends MvcConfiguration {
 
     private final signupService service;
 
     @Description("회원가입")
-    @PostMapping
+    @PostMapping("/api/sign-up")
     public ResponseEntity<ResponseJson<Object>> signUp(@RequestBody RequestSignupPostDto requestSignupPostDto){
         ResponseSavedNameDto response = service.signup(requestSignupPostDto);
 
@@ -32,4 +30,11 @@ public class signUpController {
                         .build()
         );
     }
+    @Description("회원가입 페이지로 이동")
+    @GetMapping("/sign-up-page")
+    public String signUpPage(){
+        return "sign-up-page";
+    }
+
+
 }
