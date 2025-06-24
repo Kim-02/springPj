3bafa8f1ebeb2c5128c718c0eaaf9e2f6676bb58
Kim-02 <curry2002@koreatech.ac.kr>
2025-06-24 16:36:26 +0900

    API주소 수정

--- src/main/java/com/dasolsystem/core/auth/vo/WhiteListVO.java
+++ src/main/java/com/dasolsystem/core/auth/vo/WhiteListVO.java
@@ -6,43 +6,22 @@ import lombok.*;
 @Getter
 public class WhiteListVO {
     private final String[] WhiteList={
-            "/api/signin",
-            "/api/auth/login",
-            "/api/logout",
-            "/api/auth/signup",
             "/",
-            "/api/press",
-            "/api/likes",
-
-            "/favicon.ico",
-
-            "/deposit",
-            "/test/signup",
-            "/test/main",
-            "/test/login",
-            "/index.html",
-            "/test/print",
-
-
-
-            "/api/users/upload",
-            "/api/amount/update",
-            "/api/users/personal_upload",
-            "/api/users/userdata",
-            "/api/users/deleteuser",
-            "/api/amount/download",
-            "/api/amount/personal/update",
-            "/api/users/updateuser",
-            "/api/users/personal_upload",
-            "/api/users/finduserid",
-            "/api/amount/userdata",
-            "/api/amount/refund",
-            "/api/expend/update",
-            "/check_amount/findexpender",
-
-            "/api/deposit/file/update",
+            "/api/auth/logout",
+            "/api/auth/login",
+            "/api/auth/signup",
+            "/api/users/file/upload",
+            "/api/users/personal/upload",
+            "/api/users/personal/find/id",
+            "/api/users/personal/find/data",
             "/api/deposit/personal/update",
+            "/api/users/personal/delete/user",
+            "/api/users/personal/update/role",
+            "/api/deposit/file/update",
+            "/api/deposit/find_deposit/download",
+            "/api/deposit/personal/refund",
+            "/api/expend/post",
             "/api/amount/download/amount_check",
-            "/api/users/personal/find/data"
+            "/api/announce/post"
     };
 }
