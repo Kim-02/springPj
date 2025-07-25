670905edf410f254fdb16f06bafb5fe37185d527
gyustack <ks675135@gmail.com>
2025-07-25 13:05:07 +0900

    주석달자 주석달자 주주주

--- src/main/java/com/dasolsystem/core/guardian/SecurityGuardian.java
+++ src/main/java/com/dasolsystem/core/guardian/SecurityGuardian.java
@@ -22,6 +22,9 @@ import java.security.Key;
 @Component
 @RequiredArgsConstructor
 public class SecurityGuardian {
+    /**
+     * 받아온 키(토큰) 값과 리프레쉬 토큰 값을 변수에 넣음
+     */
     @Value("${jwt.secret.key}")
     private String SecretKey; //키값임.
     @Value("${jwt.refresh.secret.key}")
@@ -37,6 +40,8 @@ public class SecurityGuardian {
      */
     public TokenResponseDto tokenValidator(String accessToken,String refreshTokenId) {
         JwtCode accessStatus = validateToken(accessToken);
+
+        //토큰이 유효할때 -> 그대로 전송함
         if(accessStatus == JwtCode.OK) {
             return TokenResponseDto.builder()
                     .jwtCode(JwtCode.OK)
@@ -44,19 +49,26 @@ public class SecurityGuardian {
                     .accessToken(accessToken)
                     .build();
         }
+        //토큰이 만료됐을 떄 -> 새로운 리프래쉬 토큰을 발급받아서 보냄
         if(accessStatus == JwtCode.EXPIRE){
+            // redis에 있는 리프레시 토큰 추출한다.
             String refreshToken = redisJwtRepository.findById(Long.valueOf(refreshTokenId)).orElseThrow(
                     () -> new DBFaillException(ApiState.ERROR_500,"DB fail ,not exist Id")
             ).getJwtToken();
             log.info("-> get refresh token from redis");
             //기존 사용하던 리프레시 토큰을 사용할 수 없게 한다.
             redisJwtRepository.deleteById(Long.valueOf(refreshTokenId));
+            //리프레쉬 토큰에 payload부분을 인출함
             Claims refreshClaims = Jwts.parserBuilder()
                     .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(RefreshSecretKey)))
                     .build()
                     .parseClaimsJws(refreshToken).getBody();
+
+            // payload에 sub 즉 학번을 가져옴
             String studentId = refreshClaims.getSubject();
             log.info("--> get refresh token subject = "+studentId);
+
+            // 엑세스 토큰에 payload 부분을 가져옴
             Claims accessClaims = Jwts.parserBuilder()
                     .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(SecretKey)))
                     .build()
@@ -72,12 +84,15 @@ public class SecurityGuardian {
                             .studentId(studentId)
                             .build()
             );
+            // 새로 만든 토큰을 리턴함
             return TokenResponseDto.builder()
                     .jwtCode(JwtCode.OK)
                     .accessToken(newAccessToken)
                     .refreshToken(String.valueOf(newRefreshTokenId))
                     .build();
         }
+
+        // if문에 걸리지 않았을떄는 오류가 있는 상태이기 때문에 null값을 보내준다.
         return TokenResponseDto.builder()
                 .jwtCode(accessStatus)
                 .message("Access token vaild failer")
@@ -86,6 +101,11 @@ public class SecurityGuardian {
                 .build();
     }
 
+    /**
+     * 엑세스 토큰을 이용해 Claim을 추출하고 Claim에서 학번 추출하는 함수임
+     * @param accessToken 학번 싸게 엑세스 토큰임
+     * @return Claim에서 학번 반환
+     */
     public String getStudentId(String accessToken) {
         Claims accessClaims = Jwts.parserBuilder()
                 .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(SecretKey)))
@@ -94,7 +114,13 @@ public class SecurityGuardian {
         return accessClaims.getSubject();
     }
 
-    public JwtCode validateToken(String token) {
+    /**
+     * 들어오는 토큰의 유효성을 검사하고, 결과를 JwtCode로 반환
+     *
+     * @param token 유효성을 검사할 토큰
+     * @return 토큰이 유효하면 JwtCode.OK, 만료됐으면 JwtCode.EXPIRE, 그 외는 JwtCode.WRONG 반환
+     */
+    public JwtCode  validateToken(String token) {
         try{
             Jwts.parserBuilder()
                     .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(SecretKey)))
@@ -123,14 +149,19 @@ public class SecurityGuardian {
      * @return Claims를 반환
      */
     public Claims getServletTokenClaims(HttpServletRequest request) {
+        // Header부분 가져옴
         String header = request.getHeader("Authorization");
+        //Header가 비어있거나 Bearer로 시작하지 않으면 null값 보냄
         if (header == null || !header.startsWith("Bearer ")) {
             return null;
         }
+
+        // 앞에 7글자 제외함 "Brearer "제외
         String token = header.substring(7);
         byte[] KeyBytes = Decoders.BASE64.decode(SecretKey);
         Key key = Keys.hmacShaKeyFor(KeyBytes);
 
+        // payload부분 추출해서 전송
         return Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
@@ -138,6 +169,13 @@ public class SecurityGuardian {
 
     }
 
+    /**
+     * 사이트에서 접근 권한이 필요할떄 접근 권한을 검사한다.
+     *
+     * @param request 클라이언트의 요청 객체
+     * @param approvalRole 서비스에 필요한 최소한의 권한
+     * @return 필요한 권한 레벨보다 요청 객체의 권한 레벨이 높다면 T 아니면 F
+     */
     public Boolean userValidate(HttpServletRequest request, String approvalRole) {
         Claims accessClaims = getServletTokenClaims(request);
         String roleName = accessClaims.get("role", String.class);
