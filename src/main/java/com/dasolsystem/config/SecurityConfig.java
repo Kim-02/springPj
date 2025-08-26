1b05719b9f99aac5c6bd3bdc41e089f8f64bda86
KimPc <curry2002@naver.com>
2025-08-26 18:49:04 +0900

    swagger 추가

--- src/main/java/com/dasolsystem/config/SecurityConfig.java
+++ src/main/java/com/dasolsystem/config/SecurityConfig.java
@@ -81,7 +81,20 @@ public class SecurityConfig {
                         "/api/auth/signup",
                         "/api/ping",
                         "/index.html",
-                        "/api/auth/verify"
+                        "/api/auth/verify",
+
+
+                        "/swagger-ui/index.html",
+                        "/swagger-ui/favicon-16x16.png",
+                        "/swagger-ui/favicon-32x32.png",
+                        "/swagger-ui/swagger-initializer.js",
+                        "/swagger-ui/swagger-ui-standalone-preset.js",
+                        "/swagger-ui/swagger-ui-bundle.js",
+                        "/swagger-ui/index.css",
+                        "/swagger-ui/swagger-ui.css",
+                        "/v3/api-docs/swagger-config",
+                        "/v3/api-docs"
+
                         ).permitAll()
                 .anyRequest().authenticated()
         );
